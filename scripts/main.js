bed5e3c96e0fb785f54790ac0d0e62382ee0dd0d
Markus <107764683+markus-vs@users.noreply.github.com>
2022-06-18 18:59:03 +0200

    My first commit
    A simple website

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/bicycle.webp') {
+      myImage.setAttribute('src','images/car.webp');
+    } else {
+      myImage.setAttribute('src','images/bicycle.webp');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'I like fast bikes my name is ' + myName;
+    }
+  }
+  
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'I like big dicks, my name is ' + storedName;
+  }
+  
+  myButton.onclick = function() {
+    setUserName();
+  }
